699eefbe23b1a2834abedfa3597c883d72b3c835
marcusikegami <mikegami.github@gmail.com>
2023-12-18 23:28:51 -0700

    braingame (v. 0.1)

--- src/views/BrainGame/BrainGame.jsx
+++ src/views/BrainGame/BrainGame.jsx
@@ -8,15 +8,15 @@ const BrainGame = () => {
     const [game, setGame] = useState(false);
     const [letters, setLetters] = useState([]);
     const [input, setInput] = useState('');
-    const [game_dictionary, setDictionary] = useState([]);
     const [wordList, setWordList] = useState([]);
 
     const startGame = () => {
         setGame(true);
-        setTime(60);
+        setTime(10);
         setScore(0);
         refreshLetters();
         setInput('');
+        setWordList([]);
 
         const timer = setInterval(() => {
             setTime((currentTime) => {
@@ -43,6 +43,7 @@ const BrainGame = () => {
             letters[i] = letter;
         }
         setLetters(letters);
+
         // disable the discard button for 2 seconds
         let discardBtn = document.getElementById('discard-button');
         if (!discardBtn) return;
@@ -54,7 +55,7 @@ const BrainGame = () => {
 
     return (
         <div>
-            <h1>Brain Game</h1>
+            <h1 className="font-mono font-semibold text-lg">Brain Game</h1>
             <p>How many words can you make in 60 seconds?</p>
 
             <div>
@@ -67,18 +68,25 @@ const BrainGame = () => {
                 ) : (
                 <section>
                     <div className="flex flex-col justify-center items-center py-3">
-                        <p className="text-lg py-3">{input}</p>
+                        <div className="flex justify-center items-center p-5 h-12">
+                            <p className="text-lg py-3">{input}</p>
+                        </div>
                         <div className="flex">
-                        {letters.map((letter) => (
+                        {letters.map((letter, index) => (
                             <button
-                                className="text-2xl mx-2"
-                                key={$`letter` + }
-                                onClick={() => {
+                                className="text-2xl mx-2 w-5"
+                                key={'letter' + index}
+                                onClick={(e) => {
                                     setInput(input + letter);
+                                    // add a flashing green border to the letter as a transition for 0.25s
+                                    e.target.classList.add('text-green-500');
+                                    setTimeout(() => {
+                                        e.target.classList.remove('text-green-500');
+                                    }, 250);
                                 }}
                             >
-                                <div className="p-1 w-5 border-b-black">
-                                    <p>
+                                <div className="p-1 w-100 border-b-black">
+                                    <p className='text-center'>
                                         {letter}
                                     </p>
                                 </div>
@@ -114,7 +122,7 @@ const BrainGame = () => {
                 </section>
                 )}
                 <section>
-                    <h2>Word List</h2>
+                    <h2 className="font-mono font-semibold text-lg mt-6">Word List</h2>
                     <ul>
                         {wordList.map((word) => (
                             <li key={word.input}>{word.input} - {word.score}</li>
